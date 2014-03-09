64a2133dc92cd60f0272b4daedf2b3e017eb1fd1
iska <iska.gp@gmail.com>
2014-03-09 20:26:38 +0100

    Create Xcode project for SimLink

---
+++ SimLink/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SimLink
+//
+//  Created by Iska on 09/03/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SimLink/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimLink
+//
+//  Created by Iska on 09/03/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
